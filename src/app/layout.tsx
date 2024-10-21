87705e0cf034fee3e7648c17297ee59dc509b34d
Umar Ali <umarcodev@gmail.com>
2024-10-21 19:16:28 +0500

    fix issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import cn from "classnames";
 import { ThemeSwitcher } from "./_components/theme-switcher";
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 import "./globals.css";
 import { Intro } from "./_components/intro";
@@ -13,8 +13,8 @@ import { Intro } from "./_components/intro";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: `Next.js Blog Example with ${CMS_NAME}`,
-  description: `A statically generated blog example using Next.js and ${CMS_NAME}.`,
+  title: `Bilal Ahmad Portfolio`,
+  description: `Bilal Ahmad Portfolio`,
   openGraph: {
     images: [HOME_OG_IMAGE_URL],
   },
@@ -28,9 +28,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="w-full p-0 m-0 overflow-x-hidden">
-      <Intro />
-        
-         {children}
+        <Intro />
+
+        {children}
         <Footer />
       </body>
     </html>
